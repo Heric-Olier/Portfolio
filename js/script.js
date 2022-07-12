1f8bc3c7f1d9970d75768703075933517359cf35
Heric Olier <hericolier.code@gmail.com>
2022-07-11 23:10:59 -0500

    burger menu improve

--- js/script.js
+++ js/script.js
@@ -1,22 +1,40 @@
 //*! Burguer Menu *//
 
-document.querySelector('.nav__menu-mobile').addEventListener('click', barsAnimation);
+let line1__bars = document.querySelector(".line1__bars-menu");
+let line2__bars = document.querySelector(".line2__bars-menu");
+let line3__bars = document.querySelector(".line3__bars-menu");
+let nav__menu = document.querySelector(".nav__menu");
+let about = document.querySelector(".about__section");
+let skills = document.querySelector(".skills__section");
+let study = document.querySelector(".study__section");
+let projects = document.querySelector(".projects__section");
+let contact = document.querySelector(".contact__section");
 
-let line1__bars = document.querySelector('.line1__bars-menu');
-let line2__bars = document.querySelector('.line2__bars-menu');
-let line3__bars = document.querySelector('.line3__bars-menu');
-let nav__menu = document.querySelector('.nav__menu');
+document
+  .querySelector(".nav__menu-mobile")
+  .addEventListener("click", barsAnimation);
 
 function barsAnimation() {
-    line1__bars.classList.toggle('active');
-    line2__bars.classList.toggle('active');
-    line3__bars.classList.toggle('active');
+  line1__bars.classList.toggle("active");
+  line2__bars.classList.toggle("active");
+  line3__bars.classList.toggle("active");
 
-    nav__menu.classList.toggle('active');
+  nav__menu.classList.toggle("active");
 }
 
+const menus = [about, skills, study, projects, contact];
 
+for (let i = 0; i < menus.length; i++) {
+  menus[i].addEventListener("click", function () {
+    nav__menu.classList.toggle("active");
+  });
+   
+}
 
-
-
-
+nav__menu.addEventListener("click", function () {
+    line1__bars.classList.toggle("active");
+    line2__bars.classList.toggle("active");
+    line3__bars.classList.toggle("active");
+    }
+    );
+    
