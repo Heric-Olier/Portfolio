7dba9f82a06a1e8c08aa0c1124f63ed4c4991b9a
Heric Olier <hericolier.code@gmail.com>
2022-08-09 09:08:10 -0500

    add Top Button in Mobile & Simplify JS Code

--- js/script.js
+++ js/script.js
@@ -1,40 +1,38 @@
-
-let navBurguer = document.querySelector(".nav__burguer");
-let navMenu = document.querySelector(".nav__menu");
-let navMenuItems = document.querySelectorAll(".nav__menu ul li");
-let navBurguerLine1 = document.querySelector(".nav__burguer-line-1");
-let navBurguerLine2 = document.querySelector(".nav__burguer-line-2");
-let navBurguerLine3 = document.querySelector(".nav__burguer-line-3");
+const navBurguer = document.querySelector(".nav__burguer");
+const navMenu = document.querySelector(".nav__menu");
+const navMenuItems = document.querySelectorAll(".nav__menu ul li");
+const navBurguerLine1 = document.querySelector(".nav__burguer-line-1");
+const navBurguerLine2 = document.querySelector(".nav__burguer-line-2");
+const navBurguerLine3 = document.querySelector(".nav__burguer-line-3");
+const topButton = document.querySelector(".top-button");
 
 //*!--------------- Burguer Menu ---------------*//
 
-// El menu se muestra y se oculta con un click en el icono de hamburguesa
-
-navBurguer.addEventListener("click", () => {
+const showNavMenu = () => {
   navMenu.classList.toggle("active");
   navBurguerLine1.classList.toggle("active");
   navBurguerLine2.classList.toggle("active");
   navBurguerLine3.classList.toggle("active");
   navBurguer.classList.toggle("active");
+};
+
+// El menu se muestra y se oculta con un click en el icono de hamburguesa
+
+navBurguer.addEventListener("click", () => {
+  showNavMenu();
 });
 
 // El menú se cierra al hacer click en un elemento del mismo
 
 navMenuItems.forEach((item) => {
   item.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-    navBurguerLine1.classList.toggle("active");
-    navBurguerLine2.classList.toggle("active");
-    navBurguerLine3.classList.toggle("active");
-    navBurguer.classList.toggle("active");
+    showNavMenu();
   });
 });
 
-//*! scroll up *//
+//*!---------------- scroll up -------------------*//
 
-let topButton = document.querySelector(".top-button");
-
-document.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
   if (window.scrollY >= 200) {
     topButton.classList.add("active");
   } else {
