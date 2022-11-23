d0e1b567b6f0cd35f09d238fcd973d07a6d1e950
Heric Olier <hericolier.code@gmail.com>
2022-11-23 16:09:39 -0500

    Improve Navbar Select Function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const navBurguer = document.querySelector(".nav__burguer");
 const navMenu = document.querySelector(".nav__menu");
-const navMenuItems = document.querySelectorAll(".nav__menu ul li");
+const navMenuItems = document.querySelectorAll(".nav__menu ul li a");
 const navBurguerLine1 = document.querySelector(".nav__burguer-line-1");
 const navBurguerLine2 = document.querySelector(".nav__burguer-line-2");
 const navBurguerLine3 = document.querySelector(".nav__burguer-line-3");
 const topButton = document.querySelector(".top-button");
+const nav = document.querySelector("nav");
 
 //*!--------------- Burguer Menu ---------------*//
 
@@ -17,35 +18,43 @@ const showNavMenu = () => {
 };
 
 // El menu se muestra y se oculta con un click en el icono de hamburguesa
-
 navBurguer.addEventListener("click", () => {
   showNavMenu();
 });
 
 // El menú se cierra al hacer click en un elemento del mismo
-
 navMenuItems.forEach((item) => {
   item.addEventListener("click", () => {
     showNavMenu();
+
+    //dejamos seleccionado el elemento del menú que se ha clickeado
+    navMenuItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+    item.classList.add("active");
   });
 });
 
-//*!--------------- scroll up & Top Button ---------------*//
+//*!--------------- scroll up & Top Button - header ---------------*//
 
-// El botón de subir se muestra y se oculta con el scroll
+// El botón de scroll up y el nav se muestra cuando se hace scroll hacia abajo
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
+  if (window.scrollY) {
     topButton.classList.add("active");
+    nav.classList.add("active");
   } else {
     topButton.classList.remove("active");
+    nav.classList.remove("active");
+    navMenuItems.forEach((item) => {
+      item.classList.remove("active");
+    });
   }
 });
 
-
 //*!--------------- Typed text ---------------*//
 
-var typed = new Typed('.hero__subtitle', {
-  strings: ['Desarrollador Web Front-End.'],
+var typed = new Typed(".hero__subtitle", {
+  strings: ["Desarrollador Web Front-End."],
   typeSpeed: 60,
   backSpeed: 60,
   backDelay: 2800,
