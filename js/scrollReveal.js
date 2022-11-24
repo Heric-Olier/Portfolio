45e97f6609dfc4ab05391b7dea25db4bb35b349f
Heric Olier <hericolier.code@gmail.com>
2022-11-24 00:49:18 -0500

    Improve Entrance Animations ScrollReveal

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,60 +1,38 @@
 // ScrollReveal({reset: true})
 
-ScrollReveal().reveal('.hero__text', {
-    origin: 'left',
-    distance: '100px',
-    duration: 1200,
+window.addEventListener("load", () => {
+    ScrollReveal().reveal('.hero__text', {origin: 'left', distance: '100px', duration: 1200, reset: true});
+    ScrollReveal().reveal('.hero__img', {origin: 'right', distance: '100px', duration: 1200, reset: true});
 });
 
-ScrollReveal().reveal('.hero__image', {
-    origin: 'right',
-    distance: '100px',
-    duration: 1200,
-});
+ScrollReveal().reveal('.hero__text', {origin: 'left', distance: '100px', duration: 1200, reset: true});
 
-ScrollReveal().reveal('.about__text', {
-    origin: 'left',
-    distance: '100px',
-    duration: 1200,
-});
+ScrollReveal().reveal('.hero__image', {origin: 'right', distance: '100px', duration: 1200, reset: true});
 
-ScrollReveal().reveal('.about__image', {
-    origin: 'right',
-    distance: '100px',
-    duration: 1200,
-});
+ScrollReveal().reveal('.about__text', {origin: 'left', distance: '100px', duration: 1200, reset: true});
 
-ScrollReveal().reveal('.skills__logo-top', {
-    origin: 'top',
-    distance: '100px',
-    duration: 1200,
-});
+ScrollReveal().reveal('.about__image', {origin: 'right', distance: '100px', duration: 1200, reset: true});
 
-ScrollReveal().reveal('.skills__logo-bottom', {
-    origin: 'bottom',
-    distance: '100px',
-    duration: 1200,
-});
+ScrollReveal().reveal('.skills__logo-top', {origin: 'top', distance: '100px', duration: 1200, reset: true});
 
-ScrollReveal().reveal('.study__card', {
-    origin: 'bottom',
-    distance: '100px',
-    duration: 1200,
-});
+ScrollReveal().reveal('.title__container', {origin: 'bottom', distance: '100px', duration: 1400, reset: true});
 
-ScrollReveal().reveal('.projects__card', {
-    scale: 0.5,
-    duration: 1000,
-});
+ScrollReveal().reveal('.title__container-study', {origin: 'bottom', distance: '100px', duration: 1400, reset: true});
 
-ScrollReveal().reveal('.contact__item', {
-    origin: 'bottom',
-    distance: '100px',
-    duration: 1200,
-});
+ScrollReveal().reveal('.skills__logo-bottom', {origin: 'bottom', distance: '100px', duration: 1200, reset: true});
 
-ScrollReveal().reveal('.contact-profile', {
-    origin: 'top',
-    distance: '100px',
-    duration: 1400,
-});
+ScrollReveal().reveal('.study__card', {origin: 'bottom', distance: '100px', duration: 1200,});
+
+ScrollReveal().reveal('.projects__card', {scale: 0.5, duration: 1000,});
+
+ScrollReveal().reveal('.contact__item', {origin: 'bottom', distance: '100px', duration: 1300, reset: true});
+
+ScrollReveal().reveal('.contact_icon-left', {origin: 'left', distance: '100px', duration: 1300, reset: true});
+
+ScrollReveal().reveal('.contact_icon-right', {origin: 'right', distance: '100px', duration: 1300, reset: true});
+
+ScrollReveal().reveal('.contact-profile', {origin: 'top', distance: '100px', duration: 1500, reset: true});
+
+ScrollReveal().reveal('.footer__reserved', {origin: 'left', distance: '100px', duration: 1300, reset: true});
+
+ScrollReveal().reveal('.footer__social', {origin: 'right', distance: '100px', duration: 1300, reset: true});
