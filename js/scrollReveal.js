abcad71842f9efc880595ef180162731b4fb8fdd
Heric Olier <hericolier.code@gmail.com>
2022-11-23 20:59:11 -0500

    Add ScrollReveal Library Entrance Animations

---
+++ js/scrollReveal.js
@@ -0,0 +1,60 @@
+ScrollReveal({reset: true})
+
+ScrollReveal().reveal('.hero__text', {
+    origin: 'left',
+    distance: '100px',
+    duration: 1200,
+});
+
+ScrollReveal().reveal('.hero__image', {
+    origin: 'right',
+    distance: '100px',
+    duration: 1200,
+});
+
+ScrollReveal().reveal('.about__text', {
+    origin: 'left',
+    distance: '100px',
+    duration: 1200,
+});
+
+ScrollReveal().reveal('.about__image', {
+    origin: 'right',
+    distance: '100px',
+    duration: 1200,
+});
+
+ScrollReveal().reveal('.skills__logo-top', {
+    origin: 'top',
+    distance: '100px',
+    duration: 1200,
+});
+
+ScrollReveal().reveal('.skills__logo-bottom', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1200,
+});
+
+ScrollReveal().reveal('.study__card', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1200,
+});
+
+ScrollReveal().reveal('.projects__card', {
+    scale: 0.5,
+    duration: 1000,
+});
+
+ScrollReveal().reveal('.contact__item', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1200,
+});
+
+ScrollReveal().reveal('.contact-profile', {
+    origin: 'top',
+    distance: '100px',
+    duration: 1400,
+});
